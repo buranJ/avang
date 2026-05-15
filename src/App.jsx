d5dd6fe5b2b322207ff4a204f1905f3477b7ec5c
begimai12 <usubeka@mail.ru>
2026-05-15 19:49:22 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 // import Home from "./pages/Home/Home";
 import Object from "./pages/Object/Object";
+import AboutComplex from "./components/Aboutcomplex/Aboutcomplex";
+import InNumbers from "./components/Innumbers/Innumbers";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
       <Object />
+      <AboutComplex />
+      <InNumbers />
     </>
   );
 }
